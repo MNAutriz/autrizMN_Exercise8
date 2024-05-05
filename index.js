8422673291264888c477482b4e240f47009687d7
MNAutriz <mgautriz@up.edu.ph>
2024-05-05 10:34:48 +0800

    Added the documentations for the structure and design of Lazado app

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// Import necessary modules and components
+import React from 'react'; // Importing React library
+import ReactDOM from 'react-dom/client'; // Importing ReactDOM library for rendering
+import './index.css'; // Importing CSS styles for the index
+import App from './App'; // Importing the main App component
 
+// Create a root element using ReactDOM.createRoot method and target the element with id 'root'
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the main App component inside a StrictMode component
 root.render(
   <React.StrictMode>
-    <App />
+    <App /> {/* Render the main App component */}
   </React.StrictMode>
 );
-
